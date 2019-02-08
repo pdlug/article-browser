55027236ce600a71a15c123bba3f7694cec042f4
Nahum Smith <nahum.smith@gmail.com>
2019-02-08 11:24:19 -0500

    Create <ArticlesContainer /> Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,37 @@ import Header from "./components/layout/Header";
 
 import Article from "./components/Article";
 
-const sampleArticle = {
-  id: "90444900-9820-448b-9780-b6fea57ac116",
-  publishedOn: "2000-10-17",
-  title: "Minima corrupti hic",
-  abstract:
-    "Laborum recusandae nesciunt earum quisquam rerum. Voluptatem velit ipsum est nesciunt cupiditate.",
-  journal: {
-    id: "A",
-    name: "Journal A"
+const sampleArticles = [
+  {
+    id: "90444900-9820-448b-9780-b6fea57ac116",
+    publishedOn: "2000-10-17",
+    title: "Minima corrupti hic",
+    abstract:
+      "Laborum recusandae nesciunt earum quisquam rerum. Voluptatem velit ipsum est nesciunt cupiditate.",
+    journal: {
+      id: "A",
+      name: "Journal A"
+    },
+    authors: ["Ari Bednar", "Cedrick Batz", "Wayne Klein", "Jasper Auer"]
   },
-  authors: ["Ari Bednar", "Cedrick Batz", "Wayne Klein", "Jasper Auer"]
+  {
+    id: "90444900-9820-448b-9780-b6fea57ac116",
+    publishedOn: "2000-10-17",
+    title: "Minima corrupti hic",
+    abstract:
+      "Laborum recusandae nesciunt earum quisquam rerum. Voluptatem velit ipsum est nesciunt cupiditate.",
+    journal: {
+      id: "A",
+      name: "Journal A"
+    },
+    authors: ["Ari Bednar", "Cedrick Batz", "Wayne Klein", "Jasper Auer"]
+  }
+];
+
+const ArticlesContainer = ({ articles }) => {
+  return articles.map(article => (
+    <Article key={article.id} article={article} />
+  ));
 };
 
 class App extends Component {
@@ -23,9 +43,8 @@ class App extends Component {
     return (
       <>
         <Header />
-
         <div className="p-8 bg-whitesmoke min-h-screen">
-          <Article article={sampleArticle} />
+          <ArticlesContainer articles={sampleArticles} />
         </div>
       </>
     );
