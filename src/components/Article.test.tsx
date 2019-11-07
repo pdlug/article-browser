4be2f1485183b128d4f30377316c0656d6d066ca
Paul Dlug <paul@aps.org>
2019-11-07 12:26:06 -0500

    Switch to typescript, use react-testing-library instead of enzyme, additional linting rules

---
+++ src/components/Article.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Article from "./Article";
+
+const article = render(<Article/>);
+
+it("should display Article Title", () => {
+  expect(false).toBe(true);
+});
+
+it("should display Article Journal Name", () => {
+  expect(false).toBe(true);
+});
+
+it("should display Article Abstract", () => {
+  expect(false).toBe(true);
+});
+
+it("should display all related Authors", () => {
+  expect(false).toBe(true);
+});
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-const Header = () => (
+const Header: React.FC = () => (
   <header className="bg-blue-600 text-white py-3 pl-4 shadow-md">
     <h1 className="text-xl font-bold">Article Browser</h1>
   </header>
